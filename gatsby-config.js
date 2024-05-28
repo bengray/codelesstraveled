1c0470256dc3179013bf65cfc38e9de28b698302
Ben Gray <bendoylegray@gmail.com>
2024-05-27 22:38:39 -0400

    GA adjustments

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,11 +152,7 @@ module.exports = {
                 // The property ID; the tracking code won't be generated without it
                 trackingId: "G-1309704R5Z",
                 // Defines where to place the tracking script - `true` in the head and `false` in the body
-                head: false,
-                // Setting this parameter is optional
-                anonymize: true,
-                // Setting this parameter is also optional
-                respectDNT: true,
+                head: true,
                 // Delays sending pageview hits on route update (in milliseconds)
                 pageTransitionDelay: 0,
                 // Defers execution of google analytics script after page load
