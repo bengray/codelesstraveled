89b17cd6aa982b33e7981bb89eef799350ad4c86
Benjamin Gray <bendoylegray@gmail.com>
2025-01-30 12:38:00 -0600

    update address

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,190 +5,189 @@
  */
 
 module.exports = {
-    pathPrefix: `/`,
-    siteMetadata: {
-        title: "Code Less Traveled",
-        titleTemplate: `Code Less Traveled`,
-        description: `Code Less Traveled develops custom web-based software applications.`,
-        author: `Ben Gray`,
-        twitterUsername: `@bgray`,
-        image: "landing.png",
-        siteUrl: "https://codelesstraveled.com",
-        canonical: "https://codelesstraveled.com",
-        getform: "https://getform.io/f/nbdoxkra",
-        copyright:
-            "Code Less Traveled LLC. All Rights Reserved.",
-        siteLanguage: "en",
-        socials: [
-            {
-                id: 1,
-                icon: "fab fa-facebook-f",
-                link: "https://www.facebook.com",
-                title: "Facebook",
-                ariaLabel: "facebook link",
-            }
-        ],
-        contact: {
-            phone: "404.775.4539",
-            email: "ben@codelesstraveled.com",
-            website: "https://codelesstraveled.com/",
-            customers: "700",
-            clients: "3200",
-            address: "Newnan, GA 30265"
-        },
-    },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-image`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-styled-components`,
-        "gatsby-transformer-json",
-        {
-            resolve: "gatsby-plugin-robots-txt",
-            options: {
-                host: "https://codelesstraveled.com",
-                sitemap: "https://codelesstraveled.com/sitemap",
-                policy: [
-                {
-                    userAgent: "*",
-                    allow: ["/"],
-                    disallow: [],
-                }
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                excerpt_separator: `<!-- endexcerpt -->`,
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 1200,
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `fonts`,
-                path: `${__dirname}/src/assets/fonts`,
-                ignore: [`**/\.*`],
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/assets/images`,
-                ignore: [`**/\.*`],
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `data`,
-                path: `${__dirname}/src/data`,
-                ignore: [`**/\.*`],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: "Code Less Traveled",
-                short_name: "CLT",
-                theme_color: "#086ad8",
-                background_color: "#ffffff",
-                display: "standalone",
-                scope: "/",
-                start_url: "/",
-                icon: "src/assets/images/favicon.png",
-                icons: [
-                    {
-                        src: "/icons/icon-72x72.png",
-                        sizes: "72x72",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-96x96.png",
-                        sizes: "96x96",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-128x128.png",
-                        sizes: "128x128",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-144x144.png",
-                        sizes: "144x144",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-152x152.png",
-                        sizes: "152x152",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-384x384.png",
-                        sizes: "384x384",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/icons/icon-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                    },
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-breadcrumb`,
-            options: {
-                useAutoGen: true,
-                autoGenHomeLabel: `Home`,
-                exclude: [`/dev-404-page`, `/404`, `/404.html`],
-                useClassNames: true,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-google-gtag`,
-            options: {
-                // You can add multiple tracking ids and a pageview event will be fired for all of them.
-                trackingIds: [
-                    "G-1309704R5Z", // Google Analytics / GA
-                ],
-                // This object gets passed directly to the gtag config command
-                // This config will be shared across all trackingIds
-                gtagConfig: {
-                    optimize_id: "OPT_CONTAINER_ID",
-                    anonymize_ip: true,
-                    cookie_expires: 0,
-                },
-                // This object is used for configuration specific to this plugin
-                pluginConfig: {
-                    // Puts tracking script in the head instead of the body
-                    head: false,
-                    // Setting this parameter is also optional
-                    respectDNT: true,
-                    // Avoids sending pageview hits from custom paths
-                    // exclude: ["/preview/**", "/do-not-track/me/too/"],
-                    // Defaults to https://www.googletagmanager.com
-                    // origin: "YOUR_SELF_HOSTED_ORIGIN",
-                    // Delays processing pageview events on route update (in milliseconds)
-                    delayOnRouteUpdate: 0,
-                },
-            },
-        },
-        "gatsby-plugin-offline"
+  pathPrefix: `/`,
+  siteMetadata: {
+    title: "Code Less Traveled",
+    titleTemplate: `Code Less Traveled`,
+    description: `Code Less Traveled develops custom web-based software applications.`,
+    author: `Ben Gray`,
+    twitterUsername: `@bgray`,
+    image: "landing.png",
+    siteUrl: "https://codelesstraveled.com",
+    canonical: "https://codelesstraveled.com",
+    getform: "https://getform.io/f/nbdoxkra",
+    copyright: "Code Less Traveled LLC. All Rights Reserved.",
+    siteLanguage: "en",
+    socials: [
+      {
+        id: 1,
+        icon: "fab fa-facebook-f",
+        link: "https://www.facebook.com",
+        title: "Facebook",
+        ariaLabel: "facebook link",
+      },
     ],
+    contact: {
+      phone: "404.775.4539",
+      email: "ben@codelesstraveled.com",
+      website: "https://codelesstraveled.com/",
+      customers: "700",
+      clients: "3200",
+      address: "Wheaton Illinois 60187",
+    },
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    "gatsby-transformer-json",
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://codelesstraveled.com",
+        sitemap: "https://codelesstraveled.com/sitemap",
+        policy: [
+          {
+            userAgent: "*",
+            allow: ["/"],
+            disallow: [],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        excerpt_separator: `<!-- endexcerpt -->`,
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `fonts`,
+        path: `${__dirname}/src/assets/fonts`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+        ignore: [`**/\.*`],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Code Less Traveled",
+        short_name: "CLT",
+        theme_color: "#086ad8",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icon: "src/assets/images/favicon.png",
+        icons: [
+          {
+            src: "/icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-breadcrumb`,
+      options: {
+        useAutoGen: true,
+        autoGenHomeLabel: `Home`,
+        exclude: [`/dev-404-page`, `/404`, `/404.html`],
+        useClassNames: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-1309704R5Z", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
 };
