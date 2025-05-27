b5cbc869578c92bb077f6500a5d44cf4a7f49a1e
Benjamin Gray <bendoylegray@gmail.com>
2025-05-27 18:51:28 -0500

    text changes... mostly

--- src/containers/hero/layout-07/index.jsx
+++ src/containers/hero/layout-07/index.jsx
@@ -5,85 +5,84 @@ import ConsultForm from "@components/forms/consult-form-02";
 import { Container, Row, Col } from "@ui/wrapper";
 import { HeadingType, ImageType } from "@utils/types";
 import {
-    StyledHero,
-    StyledTextBox,
-    StyledHeading,
-    StyledSubtitle,
-    StyledBookBox,
-    StyledBookBoxText,
-    StyledBookBoxImage,
-    StyledForm,
-    StyledImage,
-    StyledCustomHeroText,
+  StyledHero,
+  StyledTextBox,
+  StyledHeading,
+  StyledSubtitle,
+  StyledBookBox,
+  StyledBookBoxText,
+  StyledBookBoxImage,
+  StyledForm,
+  StyledImage,
+  StyledCustomHeroText,
 } from "./style";
 
 const HeroArea = ({ data }) => {
-    return (
-        <StyledHero className="modern-it-company-hero">
-            <Container>
-                <Row alignItems="center">
-                    {data?.images?.[0]?.src && (
-                        <StyledImage>
-                            <Image
-                                src={data.images[0].src}
-                                alt={data.images[0]?.alt || "Hero"}
-                            />
-                        </StyledImage>
-                    )}
-                    <Col md={10}>
-                        <StyledTextBox>
-                            {data?.headings?.[0] && (
-                                <StyledHeading
-                                    as={data.headings[0]?.level}
-                                    dangerouslySetInnerHTML={{
-                                        __html: data.headings[0]?.content,
-                                    }}
-                                />
-                            )}
-                            <Col md={7}>
-                                {data?.headings?.[1] && (
-                                <StyledCustomHeroText
-                                    as={data.headings[1]?.level}
-                                    dangerouslySetInnerHTML={{
-                                        __html: data.headings[1]?.content,
-                                    }}
-                                />
-                            )}
-                            </Col>
+  return (
+    <StyledHero>
+      <Container>
+        <Row alignItems="center">
+          {data?.images?.[0]?.src && (
+            <StyledImage>
+              <Image
+                src={data.images[0].src}
+                alt={data.images[0]?.alt || "Hero"}
+              />
+            </StyledImage>
+          )}
+          <Col md={10}>
+            <StyledTextBox>
+              {data?.headings?.[0] && (
+                <StyledHeading
+                  as={data.headings[0]?.level}
+                  dangerouslySetInnerHTML={{
+                    __html: data.headings[0]?.content,
+                  }}
+                />
+              )}
+              <Col md={8}>
+                {data?.headings?.[1] && (
+                  <StyledCustomHeroText
+                    as={data.headings[1]?.level}
+                    dangerouslySetInnerHTML={{
+                      __html: data.headings[1]?.content,
+                    }}
+                  />
+                )}
+              </Col>
 
-
-                            <StyledBookBox>
-                                <StyledBookBoxImage>
-                                    <StaticImage
-                                        src="../../../assets/images/icons/green-curve-arrow.png"
-                                        alt="curve-arrow"
-                                    />
-                                </StyledBookBoxImage>
-                                {data?.headings?.[2] && (
-                                    <StyledCustomHeroText
-                                        as={data.headings[2]?.level}
-                                        dangerouslySetInnerHTML={{
-                                            __html: data.headings[2]?.content,
-                                        }}
-                                    />
-                                )}
-                            </StyledBookBox>
-                            <StyledForm>
-                                <ConsultForm />
-                            </StyledForm>
-                        </StyledTextBox>
-                    </Col>
-                </Row>
-            </Container>
-        </StyledHero>
-    );
+              <StyledBookBox>
+                <StyledBookBoxImage>
+                  <StaticImage
+                    src="../../../assets/images/icons/green-curve-arrow.png"
+                    alt="curve-arrow"
+                  />
+                </StyledBookBoxImage>
+                {data?.headings?.[2] && (
+                  <StyledCustomHeroText
+                    as={data.headings[2]?.level}
+                    dangerouslySetInnerHTML={{
+                      __html: data.headings[2]?.content,
+                    }}
+                  />
+                )}
+              </StyledBookBox>
+              <StyledForm>
+                <ConsultForm />
+              </StyledForm>
+            </StyledTextBox>
+          </Col>
+        </Row>
+      </Container>
+    </StyledHero>
+  );
 };
 
 HeroArea.propTypes = {
-    data: PropTypes.shape({
-        headings: PropTypes.arrayOf(PropTypes.shape(HeadingType)),
-        images: PropTypes.arrayOf(PropTypes.shape(ImageType)),
-    }),
+  data: PropTypes.shape({
+    headings: PropTypes.arrayOf(PropTypes.shape(HeadingType)),
+    images: PropTypes.arrayOf(PropTypes.shape(ImageType)),
+  }),
 };
 
 export default HeroArea;
--- src/containers/hero/layout-07/style.jsx
+++ src/containers/hero/layout-07/style.jsx
@@ -1,95 +1,87 @@
 import styled, { themeGet, device } from "@styled";
 
 export const StyledHero = styled.section`
-    position: relative;
-    margin-block-end: 30px;
-    ${device.large} {
-        margin-block-end: 0;
-    }
-    ${device.xlarge} {
-        margin-block-end: 100px;
-    }
+  position: relative;
+  margin-block-end: 30px;
+  ${device.large} {
+    margin-block-end: 0;
+  }
+  ${device.xlarge} {
+    margin-block-end: 100px;
+  }
 `;
 
 export const StyledTextBox = styled.div`
-    padding-block-start: 60px;
-    position: relative;
-    z-index: 1;
-    ${device.medium} {
-        padding-block-start: 80px;
-    }
-    ${device.large} {
-        padding-block-start: 100px;
-    }
-    ${device.xlarge} {
-        padding-block-start: 100px;
-    }
+  padding-block-start: 60px;
+  position: relative;
+  z-index: 1;
 `;
 
 export const StyledHeading = styled.h1`
-    font-size: clamp(34px, 6vw, 65px);
-    line-height: 1.2;
-    mark {
-        color: ${themeGet("colors.primary")};
-        font-family: Georgia;
-    }
+  font-size: clamp(34px, 6vw, 50px);
+  line-height: 1.4;
+  mark {
+    color: ${themeGet("colors.primary")};
+    font-family: Georgia;
+  }
+  margin-block-end: 30px;
 `;
 
 export const StyledSubtitle = styled.h6`
-    margin-block-start: 30px;
+  margin-block-start: 30px;
 `;
 
 export const StyledBookBox = styled.div`
-    margin-block-start: 40px;
-    margin-block-end: 30px;
-    position: relative;
-    ${device.xlarge} {
-        margin-block-start: 100px;
-    }
+  margin-block-start: 40px;
+  margin-block-end: 30px;
+  position: relative;
+  ${device.xlarge} {
+    margin-block-start: 100px;
+  }
 `;
 
 export const StyledBookBoxImage = styled.div`
-    position: absolute;
-    top: -30px;
-    left: -80px;
-    display: none;
-    ${device.medium} {
-        display: block;
-    }
+  position: absolute;
+  top: -30px;
+  left: -80px;
+  display: none;
+  ${device.medium} {
+    display: block;
+  }
 `;
 
 export const StyledBookBoxText = styled.h5`
-    margin-block-end: 15px;
-    font-weight: 800;
-    ${device.large} {
-        max-width: 500px;
-    }
+  margin-block-end: 15px;
+  font-weight: 800;
+  ${device.large} {
+    max-width: 500px;
+  }
 `;
 
 export const StyledCustomHeroText = styled.div`
-    margin-block-start: 15px;
-    margin-block-end: 15px;
-    font-weight: 800;
-    ${device.large} {
-        max-width: 500px;
-    }
-`
+  margin-block-start: 15px;
+  margin-block-end: 15px;
+  font-weight: 800;
+  ${device.large} {
+    max-width: 700px;
+  }
+`;
 
 export const StyledForm = styled.div`
-    max-width: 550px;
+  max-width: 550px;
 `;
 
 export const StyledImage = styled.div`
+  text-align: right;
+  margin: auto;
+  margin-block-start: 60px;
+  ${device.large} {
+    text-align: center;
+  }
+  ${device.xlarge} {
+    position: absolute;
+    top: 176px;
+    right: 10px;
     text-align: right;
-    margin: auto;
-    margin-block-start: 60px;
-    ${device.large} {
-        text-align: center;
-    }
-    ${device.xlarge} {
-        position: absolute;
-        top: 176px;
-        right: 10px;
-        text-align: right;
-    }
+  }
 `;
